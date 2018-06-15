8168633e0faab23a370f7d26d9c4e2ed6fc85970
FrobozzElectric <gpfau7@gmail.com>
2018-06-15 14:27:01 -0400

    cleanup

--- decant.py
+++ decant.py
@@ -13,7 +13,6 @@ parser.add_argument('-n', '--native-command', default=None,
                     help='native command to run in the chosen prefix')
 parser.add_argument('-w', '--wine-command',
                     help='wine command to run in the chosen prefix')
-
 args = parser.parse_args()
 
 
@@ -33,19 +32,22 @@ if not app_config:
     sys.stderr.write('app not found "{}"\n'.format(args.app))
     sys.exit(1)
 
+wine_env = 'WINEPREFIX={}'.format(app_config['wine_prefix'])
+
+if 'wine_env' in app_config:
+    wine_env += ' {}'.format(app_config['wine_env'])
+
 if args.native_command:
     native_command = args.native_command
 else:
     native_command = 'wine'
 
 if args.wine_command:
-    wine_command = args.wine_command
+    wine_command = '"{}"'.format(args.wine_command)
 else:
-    wine_command = app_config['wine_command']
+    wine_command = '"{}"'.format(app_config['wine_command'])
 
-wine_env = 'WINEPREFIX={} '.format(app_config['wine_prefix'])
-
-if 'wine_env' in app_config:
-    wine_env += app_config['wine_env']
+if 'wine_command_args' in app_config:
+    wine_command += ' {}'.format(app_config['wine_command_args'])
 
 os.system('{} {} {}'.format(wine_env, native_command, wine_command))
