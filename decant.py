4fc095fd98920369e7e88b224a86acb46d29273a
FrobozzElectric <gpfau7@gmail.com>
2018-06-15 14:46:20 -0400

    list app if none is specified

--- decant.py
+++ decant.py
@@ -6,7 +6,7 @@ import yaml
 from argparse import ArgumentParser
 
 parser = ArgumentParser()
-parser.add_argument('app', help='name of app to run')
+parser.add_argument('app', nargs='?', help='name of app to run')
 parser.add_argument('-c', '--config', default='~/.decant/config.yml',
                     help='config file location')
 parser.add_argument('-n', '--native-command', default=None,
@@ -16,17 +16,27 @@ parser.add_argument('-w', '--wine-command',
 args = parser.parse_args()
 
 
-def read_user_config(user_config, app):
+def read_user_config(user_config, app=None):
     with open(user_config, 'r') as config:
         try:
             data = yaml.load(config)
-            return data.get(app)
+            if app:
+                return data.get(app)
+            else:
+                return data
         except yaml.YAMLError as error:
             sys.stderr.write(str(error))
             sys.exit(1)
 
 
-app_config = read_user_config(os.path.expanduser(args.config), args.app)
+config = os.path.expanduser(args.config)
+
+if not args.app:
+    for item in read_user_config(config):
+        sys.stderr.write('{}\n'.format(item))
+    sys.exit(0)
+
+app_config = read_user_config(config, app=args.app)
 
 if not app_config:
     sys.stderr.write('app not found "{}"\n'.format(args.app))
